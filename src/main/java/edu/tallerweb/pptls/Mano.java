804e07710fcd26451e47a5a361b79ece0a97f942
Ezequiel Aramburu <ezequi8620@gmail.com>
2013-05-06 23:44:25 -0300

    Version 1.0

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -5,23 +5,58 @@ package edu.tallerweb.pptls;
  */
 public class Mano {
 
+	private Forma forma;
+	private final Resultado[][] resultados = new Resultado[5][5];
+
 	/**
-	 * Toda Mano debe crearse con una forma dada, que ser√°
-	 * la que determine su condici√≥n en el juego.
+	 * Toda Mano debe crearse con una forma dada, que ser·
+	 * la que determine su condiciÛn en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado a√∫n");
+		this.forma = forma;
+		
+		
+		
+		resultados[Forma.LAGARTO.getValor()][Forma.LAGARTO.getValor()] = Resultado.EMPATA;
+		resultados[Forma.LAGARTO.getValor()][Forma.PAPEL.getValor()] = Resultado.GANA;
+		resultados[Forma.LAGARTO.getValor()][Forma.PIEDRA.getValor()] = Resultado.PIERDE;
+		resultados[Forma.LAGARTO.getValor()][Forma.SPOCK.getValor()] = Resultado.GANA;
+		resultados[Forma.LAGARTO.getValor()][Forma.TIJERA.getValor()] = Resultado.PIERDE;
+		
+		resultados[Forma.PAPEL.getValor()][Forma.LAGARTO.getValor()] = Resultado.PIERDE;
+		resultados[Forma.PAPEL.getValor()][Forma.PAPEL.getValor()] = Resultado.EMPATA;
+		resultados[Forma.PAPEL.getValor()][Forma.PIEDRA.getValor()] = Resultado.GANA;
+		resultados[Forma.PAPEL.getValor()][Forma.SPOCK.getValor()] = Resultado.GANA;
+		resultados[Forma.PAPEL.getValor()][Forma.TIJERA.getValor()] = Resultado.PIERDE;
+		
+		resultados[Forma.PIEDRA.getValor()][Forma.LAGARTO.getValor()] = Resultado.GANA;
+		resultados[Forma.PIEDRA.getValor()][Forma.PAPEL.getValor()] = Resultado.PIERDE;
+		resultados[Forma.PIEDRA.getValor()][Forma.PIEDRA.getValor()] = Resultado.EMPATA;
+		resultados[Forma.PIEDRA.getValor()][Forma.SPOCK.getValor()] = Resultado.PIERDE;
+		resultados[Forma.PIEDRA.getValor()][Forma.TIJERA.getValor()] = Resultado.GANA;
+		
+		resultados[Forma.SPOCK.getValor()][Forma.LAGARTO.getValor()] = Resultado.PIERDE;
+		resultados[Forma.SPOCK.getValor()][Forma.PAPEL.getValor()] = Resultado.PIERDE;
+		resultados[Forma.SPOCK.getValor()][Forma.PIEDRA.getValor()] = Resultado.GANA;
+		resultados[Forma.SPOCK.getValor()][Forma.SPOCK.getValor()] = Resultado.EMPATA;
+		resultados[Forma.SPOCK.getValor()][Forma.TIJERA.getValor()] = Resultado.GANA;
+		
+		resultados[Forma.TIJERA.getValor()][Forma.LAGARTO.getValor()] = Resultado.GANA;
+		resultados[Forma.TIJERA.getValor()][Forma.PAPEL.getValor()] = Resultado.GANA;
+		resultados[Forma.TIJERA.getValor()][Forma.PIEDRA.getValor()] = Resultado.PIERDE;
+		resultados[Forma.TIJERA.getValor()][Forma.SPOCK.getValor()] = Resultado.PIERDE;
+		resultados[Forma.TIJERA.getValor()][Forma.TIJERA.getValor()] = Resultado.EMPATA;
 	}
 
 	/**
-	 * Evaluar√° el resultado de la partida seg√∫n las reglas
+	 * Evaluar· el resultado de la partida seg˙n las reglas
 	 * del juego.
 	 * @param otra, la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado a√∫n");
+		return resultados[this.forma.getValor()][otra.forma.getValor()];
 	}
 
 }
--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -1,3 +1,6 @@
+
+
+
 package edu.tallerweb.pptls;
 
 import static org.junit.Assert.*;
@@ -11,9 +14,37 @@ public class PartidaTests {
 		Mano jugadorUno = new Mano(Forma.SPOCK);
 		Mano jugadorDos = new Mano(Forma.PIEDRA);
 
-		assertEquals("Spock vaporiza piedra",
-				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
-
+		assertEquals("Spock vaporiza piedra", Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+		assertEquals("Spock vaporiza piedra", Resultado.EMPATA, jugadorUno.jugarCon(jugadorUno));
+		assertEquals("Spock vaporiza piedra", Resultado.EMPATA, jugadorDos.jugarCon(jugadorDos));
+		assertEquals("Spock vaporiza piedra", Resultado.PIERDE, jugadorDos.jugarCon(jugadorUno));
 	}
+	
+	@Test
+	public void quePapelCubrePiedra() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PIEDRA);
+
+		assertEquals("Papel cubre piedra", Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+		assertEquals("Papel cubre piedra", Resultado.EMPATA, jugadorUno.jugarCon(jugadorUno));
+		assertEquals("Papel cubre piedra", Resultado.EMPATA, jugadorDos.jugarCon(jugadorDos));
+		assertEquals("Papel cubre piedra", Resultado.PIERDE, jugadorDos.jugarCon(jugadorUno));
+	}
+
+
+
+
+@Test
+public void quePiedraAplastaLagarto() {
+
+	Mano jugadorUno = new Mano(Forma.PIEDRA);
+	Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+	assertEquals("Piedra aplasta lagarto", Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+	assertEquals("Piedra aplasta lagarto", Resultado.EMPATA, jugadorUno.jugarCon(jugadorUno));
+	assertEquals("Piedra aplasta lagarto", Resultado.EMPATA, jugadorDos.jugarCon(jugadorDos));
+	assertEquals("Piedra aplasta lagarto", Resultado.PIERDE, jugadorDos.jugarCon(jugadorUno));
+}
 
 }
